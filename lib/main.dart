669f3f8626e97954bb5b5e1ae5af83fb6dc87b7e
Your Name <abc.@gmail.com>
2022-02-13 10:22:23 +0530

    My first app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp(),));
+}
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      title: Text('My First App'),
+      backgroundColor: Colors.grey,
+    ),
+    
+    
+
+    );
+  }
+}
