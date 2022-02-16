81f7699623f87c11b88401dfd8bafe0aba41538f
Your Name <abc.@gmail.com>
2022-02-16 23:07:23 +0530

    Add two Screen via MaterialPagerouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp(),));
+  runApp(const MaterialApp(home: MyApp(),));
 }
 
 class MyApp extends StatelessWidget {
-
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
-      title: Text('My First App'),
+      title: const Text('My First App'),
       backgroundColor: Colors.lightGreenAccent,
     ),
-    body: Container(child: Text('Hye Abhay'),),
+    body: Center(child: ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) =>const SndScr(),));}, child: const Text('SndScreen')),)
      );
+  }
+}
+class SndScr extends StatelessWidget {
+const SndScr({Key? key}) : super(key: key);
+
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
   }
 }
