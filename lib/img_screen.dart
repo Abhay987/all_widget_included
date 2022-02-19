08df6682a1832005b2a7a98e6541532fb30a8c34
Your Name <abc.@gmail.com>
2022-02-19 15:13:43 +0530

    Add images & different images stored

--- lib/img_screen.dart
+++ lib/img_screen.dart
@@ -6,10 +6,26 @@ class ImgScr extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title: const Text('Image Screen'),),
     body: Center(
-      child: Container(width: 600,
-        height: 600,
-        color: Colors.cyanAccent,
-        child: Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',alignment: Alignment.centerRight,),
+      child: Container(
+        width: 300,
+        height: 300,
+   color: Colors.cyanAccent,
+       /* child: Image.network('https://tech.pelmorex.com/wp-content/uploads/2020/10/flutter-1030x488.png',
+        alignment: Alignment.bottomRight,
+        colorBlendMode: BlendMode.colorBurn,
+        fit: BoxFit.contain,
+        repeat: ImageRepeat.noRepeat,
+        ),*/
+       // child: Image.asset('assets/images/f1.png'),
+       /*decoration: BoxDecoration(
+              color: Colors.cyanAccent,      
+              image:const DecorationImage(image: NetworkImage('https://tech.pelmorex.com/wp-content/uploads/2020/10/flutter-1030x488.png'),
+              //fit: BoxFit.cover,
+              ),             
+         borderRadius: BorderRadius.circular(200),
+       ),
+*/
+child: ClipOval(child: Image.asset('assets/images/f1.png',fit: BoxFit.cover,),),
 
       ),
     ),
